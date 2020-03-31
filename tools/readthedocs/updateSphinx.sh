3de6168ffea4b776978e7b392cc094da2f2ed58a
adam <a.ellery@uq.edu.au>
2020-03-31 17:16:08 +1000

    Chaned depth to 4 in Readthedocs index.rst

--- tools/readthedocs/updateSphinx.sh
+++ tools/readthedocs/updateSphinx.sh
@@ -1,4 +1,5 @@
 # create index.rst
 python3 `pwd`/doc/sphinx_api/genrst.py `pwd`/escript esys `pwd`/tools/readthedocs/api
-#copy over conf.py
-cp `pwd`/doc/sphinx_api/conf.py `pwd`/tools/readthedocs/api/conf.py
+cp `pwd`/tools/readthedocs/api/index.rst `pwd`/doc/sphinx_api/index.rst 
+
+
